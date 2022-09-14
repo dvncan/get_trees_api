469489e70815707d2c7793b5fb607aab22e482df
dvncan <dunbrown64@gmail.com>
2022-09-14 19:11:35 -0400

    commit

---
+++ mods/blockfrost.js
@@ -0,0 +1,127 @@
+require('dotenv').config();
+const Blockfrost = require("@blockfrost/blockfrost-js");
+// import { BlockFrostAPI } from '@blockfrost/blockfrost-js'; // using import syntax
+
+const API = new Blockfrost.BlockFrostAPI({
+  projectId: "mainnetaDjfYeFpWDLijhepv0UPxPoVu71t5dV1" // see: https://blockfrost.io
+});
+
+const VERITREE_TOKEN_POLICYID = "f7c777fdd4531cf1c477551360e45b9684073c05c2fa61334f8f9add5665726974726565546f6b656e";
+const VERITREE_DONATION_POLICYID = "56b10a2a34fa7be327f28958cbb794c649986f4cf4a1d7da065dfd7c";
+const VERITREE_TEST_STAKEID = "stake1u97ez423f87gz7x7vulcxwh702vzqwwa0ldlh6gurf6mjss73y8p7";
+
+async function stakeId_addresses(stakeId){
+    try{
+        const data = await API.accountsAddresses(stakeId);
+        //console.log(data);
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function stakeId_assets(stakeId){
+    try{
+        const data = await API.accountsAddressesAssets(stakeId);
+        //console.log(data);
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function stakeId_assets_test(){
+    try{
+        const data = await API.accountsAddressesAssets("stake1u97ez423f87gz7x7vulcxwh702vzqwwa0ldlh6gurf6mjss73y8p7");
+        //console.log(data);
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function assetId_info(assetId){
+    try{
+        const data = await API.assetsById(assetId);
+        //console.log(data);
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function assetId_info_test(){
+    try{
+        //const data = await API.assetsById("56b10a2a34fa7be327f28958cbb794c649986f4cf4a1d7da065dfd7c445230363832")
+        const data = await API.assetsById("f7c777fdd4531cf1c477551360e45b9684073c05c2fa61334f8f9add5665726974726565546f6b656e")
+        // console.log(data.onchain_metadata);
+        // console.log(data);
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+// allAddressAssetUtxo('')
+
+async function allAddressAssetUtxo(address, asset){
+    try{
+        const data = await API.addressesUtxosAssetAll(address, asset);
+        //console.log(data);
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function latestBlock(){
+    try{
+        const data = await API.blocksLatest();
+        //console.log(data);
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function getAssetByAssetID(assetId){
+    try{
+        const data = await Blockfrost.parseAsset(assetId);
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function runExample() {
+  try {
+    const latestBlock = await API.blocksLatest();
+    const networkInfo = await API.network();
+    const latestEpoch = await API.epochsLatest();
+    const health = await API.health();
+    const address = await API.addresses(
+      "addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz"
+    );
+    const pools = await API.pools({ page: 1, count: 10, order: "asc" });
+
+    console.log("pools", pools);
+    console.log("address", address);
+    console.log("networkInfo", networkInfo);
+    console.log("latestEpoch", latestEpoch);
+    console.log("latestBlock", latestBlock);
+    console.log("health", health);
+  } catch (err) {
+    console.log("error", err);
+  }
+}
+
+// runExample();
+
+
+
+//get_trees(VERITREE_TEST_STAKEID);
+
+//assetId_info_test();
+//stakeId_assets_test();
+//get_trees(VERITREE_TEST_STAKEID);
+module.exports = {runExample, getAssetByAssetID, assetId_info, stakeId_assets, latestBlock, assetId_info_test, stakeId_assets_test, stakeId_addresses, allAddressAssetUtxo};
---
+++ mods/nftrees.js
@@ -0,0 +1,75 @@
+var blockfrost = require('./blockfrost');
+
+const VERITREE_TOKEN_POLICYID = "f7c777fdd4531cf1c477551360e45b9684073c05c2fa61334f8f9add5665726974726565546f6b656e";
+const VERITREE_DONATION_POLICYID = "56b10a2a34fa7be327f28958cbb794c649986f4cf4a1d7da065dfd7c";
+const VERITREE_TEST_STAKEID = "stake1u97ez423f87gz7x7vulcxwh702vzqwwa0ldlh6gurf6mjss73y8p7";
+const VERITREE_ASSETID="56b10a2a34fa7be327f28958cbb794c649986f4cf4a1d7da065dfd7c445230363837";
+
+
+async function get_trees(stakeId) {
+    try{
+        const data = await blockfrost.stakeId_assets(stakeId);
+        const policyId = VERITREE_DONATION_POLICYID;
+        const veritreeTokenPolicyId = VERITREE_TOKEN_POLICYID;
+        const treeList = [];
+
+        //console.log(policyId.length);
+        
+        var treesTotal = 0;
+        
+        for(i in data){
+            let res = data[i].unit;
+            //console.log(res);
+            //console.log(res.length);
+            
+            let first56 = res.substring(0, policyId.length);
+            
+            console.log("Comparing " + policyId + " /****/ " + first56);
+            
+            if(policyId === first56){
+                /*let trees = data.onchain_metadata['Trees Planted'];
+                console.log(data.onchain_metadata['Trees Planted']);
+                treeList.push(trees);
+                treesTotal += trees;*/
+                console.log("******************MATCH******************")
+                let data2 = await blockfrost.assetId_info(res);
+                
+                let trees = parseInt(data2.onchain_metadata['Trees Planted']);
+                let plantingSite = data2.onchain_metadata['Planting Site'];
+                console.log(plantingSite);
+                console.log(trees + " added to list");
+                let treeObj = {
+                    plantingSite: plantingSite.Name,
+                    treesPlanted: trees,
+                    treesGPS: "Longitude: " + plantingSite.Longitude + " & Latitude: " + plantingSite.Latitude
+                }
+                console.log(treeObj);
+                treeList.push(treeObj);
+                treesTotal += trees;
+            }else{
+                console.log(policyId + " != " + first56);
+            }
+        }
+        console.log("Total Trees = " + treesTotal);
+        console.log(treeList.length);
+        (treeList.forEach(element => {
+            console.log("***************DONATION RECORD************************");
+            console.log("Planting Site: " + element.plantingSite);
+            console.log("Number of trees: " + element.treesPlanted);
+            console.log("Planting Location: " + element.treesGPS);
+            console.log("******************************************************\n");
+        }));
+        if (treesTotal===0){
+            treeList.push({totalTrees: 0});
+        }else{
+            treeList.push({totalTrees: treesTotal})
+        }
+        return treeList;
+    }catch(err){
+        console.log("here" + err);
+    }
+}
+
+//get_trees(VERITREE_TEST_STAKEID);
+
+module.exports = {get_trees}
